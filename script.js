c7707d6af20fcdec2d90713eaf13c60ca065fbe9
miguelssprudencio <miguel_s_prudencio@estudante.sesisenai.org.br>
2026-05-23 16:49:03 -0300

    IMC Calculator

--- script.js
+++ script.js
@@ -34,7 +34,20 @@ function handleButtonClick() {
     
     altura = altura / 100
     let imc = peso / (altura * altura)
-    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m²`)
+    
+if (imc < 18.5) {
+    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m², abaixo do peso!`)
+}else if (imc >= 18.5 && imc <= 24.9) {
+    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m², peso saudável!`)
+}else if (imc >= 25.0 && imc <= 29.9) {
+    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m², sobrepeso!`)
+}else if (imc >= 30.0 && imc <= 34.9) {
+    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m², obesidade grau 1!`)
+}else if (imc >= 35.0 && imc <= 39.9) {
+    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m², obesidade grau 2!`)
+}else if (imc > 40.0) {
+    alert(`Seu imc equivale a ${imc.toFixed(2)}kg/m², obesidade grau 3!`)
+}
   }while (confirm("Deseja tentar novamente?"))
 }
 
